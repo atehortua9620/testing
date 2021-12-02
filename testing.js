b3915f9b8c5a8aa2196800197a0b811e369427db
Andres Atehortua <atehortua9620@gmail.com>
2021-12-02 17:25:53 -0500

    add suma function

--- testing.js
+++ testing.js
@@ -1,6 +1,12 @@
 const hola = (number_1, number_2)=>{
-    number_1 > number_2
+    return number_1 > number_2
 }
 
 console.log(hola(2,3));
 console.log(hola(5,2));
+
+const Adios =(uno, dos)=>{
+    return uno +dos;
+}
+
+console.log("suma",Adios(3,2))
